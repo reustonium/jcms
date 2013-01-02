f05ea4c7bf21dfabb3425df7d93fee52b5e068de
AndrewRuestow <andy@sharpsyntax.com>
2013-01-02 14:58:24 -0500

    added readme

--- chrome-ext/src/scripts/context.js
+++ chrome-ext/src/scripts/context.js
@@ -1,8 +1,6 @@
 // A generic onclick callback function.
-function genericOnClick(info, tab) {
-  console.log("item " + info.menuItemId + " was clicked");
-  console.log("info: " + JSON.stringify(info));
- // console.log("tab: " + JSON.stringify(tab));
+function genericOnClick(info) {
+  console.log(info.srcUrl);
 }
 
 var title = "Add to JC Queue";
