2e13c45c9f9f1d38a81414f838f1d41c47c7cef2
AndrewRuestow <andy@sharpsyntax.com>
2013-01-07 22:34:24 -0500

    updated createAlbum
    [new] Create's Album closes #1
    [new] Create's Photos closes #2
    [new] pulls photos from DB
    [new] updated DB after posting photos

--- php-script/createAlbum.php
+++ php-script/createAlbum.php
@@ -3,12 +3,20 @@
     $appId = '238552796278527';
     $appSecret = '83f3fa6948e194c5da23ca7297df6d02';
     $appUrl = 'http://alpineindie.com/jcms/createAlbum.php';
+    $pageName = 'Reustonium';
 
     session_start();
 
     $facebook = new Facebook(array(
                                   'appId'=>$appId,
                                   'secret'=>$appSecret));
+
+    // mySQL Connection
+    $con = mysql_connect("localhost:3306","aplinein_jc","pizzacake");
+    if (!$con){
+      die('Could not connect: ' . mysql_error());
+    }
+    mysql_select_db("aplinein_dailybread", $con);
 ?>
 
 <html>
@@ -37,34 +45,49 @@
             $params = null;
             parse_str($response, $params);
 
-            // Add cURL Code or php-sdk method to call graph-api
+            // Set session to User Access Token to find Page Access
             $facebook->setAccessToken($params['access_token']);
-
-            $graph_url = "https://graph.facebook.com/me?access_token=" 
-            . $params['access_token'];
-
-            $user = json_decode(file_get_contents($graph_url));
-            echo("<br>Hello " . $user->name);
-
-            $ret_obj = $facebook->api('/me/feed', 'POST',
-                                    array(
-                                      'link' => 'www.example.com',
-                                      'message' => 'Posting with the PHP SDK!'
-                                 ));
-            echo '<br><pre>Post ID: ' . $ret_obj['id'] . '</pre>';
-
             $pages = $facebook->api('/me/accounts?fields=name,id','GET');
 
-            $pageID = $pages['data'][0]['id'];
-
+            // Find the ID for the proper page
+            foreach($pages['data'] as $page){
+              if ($page['name']==$pageName){
+                echo($page['name'].'<br>'.$page['id']);
+                $pageID = $page['id'];
+              } else {echo('no page match');}
+            }
+           
+            // Update the Access Token for the Page
             $pageToken = $facebook->api('/'.$pageID.'?fields=access_token','GET');
             $facebook->setAccessToken($pageToken['access_token']);
-            $newAlbumId = $facebook->api('/me/albums','POST', array('name'=>'fresh','description'=>'stuffz'));
-            $newPhoto = $facebook->api('/'.$newAlbumId['id'].'/photos','POST',array('url'=>'http://profile.ak.fbcdn.net/hprofile-ak-ash4/369308_502360721_167399057_q.jpg','name'=>'drewski'));
- 
+            
+            // Create New Album
+            $album = $facebook->api('/me/albums','POST',array('name'=>'Cheeseburger Daily Bread '.date('F j, Y - H:i'), 'description'=>'Daily Bread'));
+            
+            // Add Photos and update DB
+            $result = mysql_query("SELECT * FROM daily_bread WHERE album_ID IS NULL LIMIT 2");
+            while($row = mysql_fetch_array($result))
+            {
+              echo "<br />";
+              echo 'comment: '.$row['comment']. ' uid: '.$row['uid'];
+          
+              $photo = $facebook->api('/'.$album['id'].'/photos','POST',array('url'=>$row['url'],'name'=>$row['comment']));
+
+              // update DB
+              mysql_query("UPDATE daily_bread SET album_ID='".$album['id']."' WHERE uid=".$row['uid']);
+            } 
+
+
+            // Create Post for Front Page
+            $postInfo = $facebook->api('/'.$album['id'].'/?fields=link,name','GET');
+            $post = $facebook->api('/me/feed','POST',array('link'=>$postInfo['link']));
+
+            // Send email to JC
           } else {  
             echo("The state does not match. You may be a victim of CSRF.");
           }
+
+          mysql_close($con);
     ?>
   </body>
 </html>
